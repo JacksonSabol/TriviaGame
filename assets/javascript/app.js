1dab043199c091e093a02ca992c8742bb991f28c
Jackson Sabol <JacksonSabol@users.noreply.github.com>
2018-11-15 19:10:43 -0800

    add questions and answers arrays to JS; add then immediately remove failed button styling

---
+++ assets/javascript/app.js
@@ -0,0 +1,58 @@
+// Array of questions
+var questionsArray = [
+    "How old is our solar system?", // 1
+    "How far away is the Earth from the Sun?", // 2
+    "How many planets are in our solar system?", // 3
+    "What is the name of the star at the center of our closest, neighboring solar system?", // 4
+    "What is the brightest star in the night sky?", // 5
+    "How long does it take Mercury to complete one revolution around our Sun?", // 6
+    "What is the largest planet, in terms of volume, in our solar system?", // 7
+    "What are the names of the 2 moons orbitting Mars?", // 8
+    "How many principal rings does Neptune have?", // 9
+    "Which one of Jupiter's moons has liquid water?" //10
+];
+
+// Array of answers
+var answersArray {
+    questionOne: {
+        correctAnswer: "4.6 billion years old",
+        incorrectAnswers: ["10,000 years old", "4.6 million years old", "13.6 billion years old"]
+      },
+    questionTwo: {
+        correctAnswer: "93 million miles",
+        incorrectAnswers: ["93 billion miles", "238,900 miles", "33.9 million miles"]
+    },
+    questionThree: {
+        correctAnswer: "8 planets",
+        incorrectAnswers: ["9 planets", "10 planets", "7 planets"]
+    },
+    questionFour: {
+        correctAnswer: "Proxima Centauri",
+        incorrectAnswers: ["Polaris", "Betelgeuse", "Rigel"]
+    },
+    questionFive: {
+        correctAnswer: "Sirius",
+        incorrectAnswers: ["Arcturus", "Canopus", "Altair"]
+    },
+    questionSix: {
+        correctAnswer: "88 days",
+        incorrectAnswers: ["165 years", "225 days", "687 days"]
+    },
+    questionSeven: {
+        correctAnswer: "Jupiter",
+        incorrectAnswers: ["Saturn", "Neptune", "Uranus"]
+    },
+    questionEight: {
+        correctAnswer: "Phobos and Deimos",
+        incorrectAnswers: ["Pheobe and Deimos", "Phobos and Io", "Pheobe and Dione"]
+    },
+    questionNine: {
+        correctAnswer: "5",
+        incorrectAnswers: ["0", "3", "1"]
+    },
+    questionTen: {
+        correctAnswer: "Europa",
+        incorrectAnswers: ["Io", "Callisto", "Elara"]
+    }
+};
+
